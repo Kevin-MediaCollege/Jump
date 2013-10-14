94910d96f0d7bc9cb196d86a7676f7d7cd0db239
Snakybo (Laptop) <krol.kevin@gmail.com>
2013-10-14 10:50:27 +0200

    Added basic project and Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player:MonoBehaviour {
+	private int walkSpeed = 10;
+	private int jumpSpeed = 30;
+	
+	private bool onGround;
+	
+	void FixedUpdate() {
+		Vector3 speed = new Vector3(0, rigidbody.velocity.y - 1, 0);
+		
+		if(Input.GetKey("d")) {
+			if(!onGround) {
+				speed.x = walkSpeed / 2;
+			} else {
+				speed.x = walkSpeed;
+			}
+		} else if(Input.GetKey("a")) {
+			if(!onGround) {
+				speed.x = -walkSpeed / 2;
+			} else {
+				speed.x = -walkSpeed;
+			}
+		}
+			
+		if(Input.GetKey("space") && onGround) {
+			speed.y = jumpSpeed;
+			onGround = false;
+		}
+		
+		rigidbody.velocity = speed;
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		if(collision.contacts[0].normal.y > 0.7f) {
+			onGround = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+	public int direction;
+	
+	private int rollingSpeed = 5;
+	private bool onGround;
+	
+	void FixedUpdate() {
+		Vector3 speed = new Vector3(0, rigidbody.velocity.y - 1, 0);
+		
+		if(onGround) {
+			if(direction > 0) {
+				speed.x = rollingSpeed;
+			} else if(direction < 0) {
+				speed.x = -rollingSpeed;
+			}
+		}
+		
+		rigidbody.velocity = speed;
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		if(collision.contacts[0].normal.y > 0.7f) {
+			onGround = true;
+		}	
+	}
+}
